d0bbd129ac729001851510a5aeffc126f6c8fe14
Aditeya Baral <aditeya.baral@gmail.com>
2021-05-13 15:04:10 +0530

    change DB URL

--- app/redirector.py
+++ app/redirector.py
@@ -7,7 +7,7 @@ from flask_sqlalchemy import SQLAlchemy
 dotenv.load_dotenv()
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ["DB_KEY"]
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
